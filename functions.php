f4ca284b3e758c1003e21a4aca09d5a789c3e78c
BlueSuperGlue <39532573+BlueSuperGlue@users.noreply.github.com>
2021-04-22 12:34:53 -0500

    restored filter for random exibits

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ function random_featured_exhibit()
     } else {
         $html .= '<p>' . __('You have no featured exhibits.') . '</p>';
     }
-    
+    $html = apply_filters('exhibit_builder_display_random_featured_exhibit', $html);
     return $html;
 }
 
